296e86cc7a20c776f893592ceda7b21fef01e271
YANGZ001 <zyfunnyyoung@gmail.com>
2022-01-11 12:07:01 +0800

    Initial commit. Build from the very beginning.

---
+++ src/main/java/com/yangz/bloblog/BloblogApplication.java
@@ -0,0 +1,13 @@
+package com.yangz.bloblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yangz/bloblog/BloblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangz.bloblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
